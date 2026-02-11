3996db56d10612ec1032783faedc462141b4b50b
Rishabh Kumar <rishabhgzp2004@gmail.com>
2026-02-11 13:32:34 +0530

    admin ui docter verification

---
+++ src/app/(main)/doctor/verification/page.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const VerificationPage = async() => {
+   // const user = await getCurrentUser();
+ 
+    //if already verified,redirect to dashboard
+
+    // if(user?.verificationStatus === "VERIFIED"){
+    //     redirect("/doctor");
+    // }
+
+    // const isRejected = user?.verificationStatus==="REJECTED";
+
+    
+
+  return (
+    <div>
+      VerificationPage
+    </div>
+  )
+}
+
+export default VerificationPage
